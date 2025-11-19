89a4e22b09a6d593f4ab4b5def41e9c25c6b06d4
paulof9 <above2@Outlook.com>
2025-11-19 16:21:50 -0300

    refact&docs: Ajusta código com GPT e eventuais dúvidas são explicadas em comentários ao longo do código

--- Lista.h
+++ Lista.h
@@ -2,24 +2,36 @@
 #define LISTA_H
 #define MAX 100
 
-struct aluno {
-	int mat;
+typedef struct aluno {
+    int mat;
     char nome[30];
-    int nota1, nota2, nota3
-};
+    float nota1;
+    float nota2;
+    float nota3;
+} Aluno;
 
 typedef struct lista Lista;
 
-// Funções
-Lista *cria_lista();
-int adicionar_inicio(Lista *li);
-int adicionar_meio(Lista *li);
-int adicionar_fim(Lista *li);
-int adicionar_pos(Lista *li, int pos);
+// Criação e destruição
+Lista *cria_lista(void);
+void liberar_mem(Lista **li);
+
+// Consultas
+int lista_vazia(const Lista *li);
+int lista_cheia(const Lista *li);
+
+// Inserções
+int adicionar_inicio(Lista *li, Aluno aluno);
+int adicionar_meio(Lista *li, Aluno aluno);
+int adicionar_fim(Lista *li, Aluno aluno);
+int adicionar_pos(Lista *li, int pos, Aluno aluno);
+
+// Remoções
 int remover_inicio(Lista *li);
 int remover_meio(Lista *li);
 int remover_final(Lista *li);
 int remover_pos(Lista *li, int pos);
-int lista_vazia(Lista *li);
-void liberar_mem(Lista **li);
+
+// Utilidades
+void exibir_lista(const Lista *li);
 #endif
